08ba495e9d44586886acff03856903087184d63e
mchoiruln <mchoiruln@gmail.com>
2021-10-21 23:35:32 +0700

    fix: infinite redirect for register route

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,26 @@
 // WARNING: this middleware only works in client-side
 
 export default function ({ store, route, redirect, $fire }) {
+  // find firebase access token from localStorage
   const userIsLoggedIn = !!localStorage.getItem('token')
 
-  if (!userIsLoggedIn && route.name.toLowerCase() !== 'login') {
+  // redirect if user not login
+  if (
+    !userIsLoggedIn &&
+    route.name.toLowerCase() !== 'login' &&
+    route.name.toLowerCase() !== 'register'
+  ) {
+    // console.log('redirect to login')
     return redirect('/login')
   }
 
-  if (userIsLoggedIn && route.name.toLowerCase() === 'login') {
+  // if authenticated redirect to index
+  if (
+    userIsLoggedIn &&
+    (route.name.toLowerCase() === 'login' ||
+      route.name.toLowerCase() === 'register')
+  ) {
+    // console.log('redirect to index')
     return redirect('/')
   }
 }
